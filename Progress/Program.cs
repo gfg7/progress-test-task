cc41e64d32104987769a89b3e5d119614b7cf59f
gfg7 <nakonechnaya040702@gmail.com>
2023-05-10 05:28:22 +0500

    set di

--- Progress/Program.cs
+++ Progress/Program.cs
@@ -1,17 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Progress.Interfaces;
+using Progress.Services;
 using Progress.Services.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DBContext>();
+builder.Services.AddDbContext<DbContext,DBContext>();
+builder.Services.AddScoped(typeof(IRepository<,>),typeof(Repository<,>));
+builder.Services.AddScoped<IMBKDictionary, MBKRepository>();
+builder.Services.AddScoped(typeof(IExport<>),typeof(ExportXmlService<>));
+builder.Services.AddScoped<PatientManagementService>();
+builder.Services.AddScoped<VisitManagementService>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior",true);
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/patients/error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -25,6 +35,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Patient}/{action=Index}/{id?}");
 
 app.Run();
