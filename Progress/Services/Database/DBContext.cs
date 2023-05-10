ddd28d56bfeb0a41a05aa54577b0576e134235f9
gfg7 <nakonechnaya040702@gmail.com>
2023-05-10 05:24:22 +0500

    move to postgresql

--- Progress/Services/Database/DBContext.cs
+++ Progress/Services/Database/DBContext.cs
@@ -5,17 +5,27 @@ namespace Progress.Services.Database
 {
     public class DBContext : DbContext
     {
-        public DbSet<Patient> Patients{get;set;}
-        public DbSet<Visit> Visits{get;set;}
-        protected DBContext()
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Visit> Visits { get; set; }
+        public DbSet<Diagnosis> Diagnoses { get; set; }
+
+        public DBContext()
         {
-            Database.EnsureCreated();
+            try
+            {
+                Database.EnsureCreated();
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
             this.ChangeTracker.LazyLoadingEnabled = false;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-             optionsBuilder.UseOracle(Environment.GetEnvironmentVariable("DB_CONN"));
+            optionsBuilder
+            .UseNpgsql(Environment.GetEnvironmentVariable("DB_CONN"));
         }
     }
 }
