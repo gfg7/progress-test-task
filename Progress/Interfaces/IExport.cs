2d4de39017be8d1769ed1f241f3b32b80eb16554
gfg7 <nakonechnaya040702@gmail.com>
2023-05-10 05:26:03 +0500

    add xml export service

--- Progress/Interfaces/IExport.cs
+++ Progress/Interfaces/IExport.cs
@@ -1,8 +1,8 @@
 using System.Threading.Tasks;
 namespace Progress.Interfaces
 {
-    public interface IExport<O,I>
+    public interface IExport<O>
     {
-        Task<O> Export(I obj);
+        Task<O> Export<I>(I obj);
     }
 }
--- Progress/Services/ExportXmlService.cs
+++ Progress/Services/ExportXmlService.cs
@@ -1,19 +1,24 @@
+using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using Progress.Interfaces;
 
 namespace Progress.Services
 {
-    public class ExportXmlService<T> : IExport<string, T>
+    public class ExportXmlService<T> : IExport<string>
     {
-        public Task<string> Export(T obj)
+        public Task<string> Export<T>(T obj)
         {
             var serializer = new XmlSerializer(typeof(T));
             var xml = string.Empty;
+            var settings = new XmlWriterSettings()
+            {
+                Encoding = Encoding.UTF8
+            };
 
             using (var stringWriter = new StringWriter())
             {
-                using (var stream = XmlWriter.Create(stringWriter))
+                using (var stream = XmlWriter.Create(stringWriter, settings))
                 {
                     serializer.Serialize(stream, obj);
                     xml = stringWriter.ToString();
