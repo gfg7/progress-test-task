c1da6c9e76c5c01c1d8775e807cf54906bd4b59c
gfg7 <nakonechnaya040702@gmail.com>
2023-05-10 05:28:51 +0500

    add db migration

---
+++ Progress/Migrations/20230509230014_initial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Progress.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diagnoses",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ParentId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diagnoses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    Surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Patronymic = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visits",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    Time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Diagnosis = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    PatientId = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Visits_Diagnoses_Diagnosis",
+                        column: x => x.Diagnosis,
+                        principalTable: "Diagnoses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Visits_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_Diagnosis",
+                table: "Visits",
+                column: "Diagnosis");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_PatientId",
+                table: "Visits",
+                column: "PatientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Visits");
+
+            migrationBuilder.DropTable(
+                name: "Diagnoses");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
---
+++ create_schema.sql
@@ -0,0 +1,74 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20230509230014_initial') THEN
+    CREATE TABLE "Diagnoses" (
+        "Id" character varying(36) NOT NULL,
+        "Name" text NOT NULL,
+        "ParentId" text NOT NULL,
+        CONSTRAINT "PK_Diagnoses" PRIMARY KEY ("Id")
+    );
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20230509230014_initial') THEN
+    CREATE TABLE "Patients" (
+        "Id" character varying(36) NOT NULL,
+        "Surname" character varying(50) NOT NULL,
+        "Name" character varying(50) NOT NULL,
+        "Patronymic" character varying(50) NULL,
+        "DateOfBirth" timestamp with time zone NOT NULL,
+        "PhoneNumber" character varying(15) NULL,
+        CONSTRAINT "PK_Patients" PRIMARY KEY ("Id")
+    );
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20230509230014_initial') THEN
+    CREATE TABLE "Visits" (
+        "Id" character varying(36) NOT NULL,
+        "Time" timestamp with time zone NOT NULL,
+        "Diagnosis" character varying(36) NOT NULL,
+        "PatientId" character varying(36) NOT NULL,
+        CONSTRAINT "PK_Visits" PRIMARY KEY ("Id"),
+        CONSTRAINT "FK_Visits_Diagnoses_Diagnosis" FOREIGN KEY ("Diagnosis") REFERENCES "Diagnoses" ("Id") ON DELETE CASCADE,
+        CONSTRAINT "FK_Visits_Patients_PatientId" FOREIGN KEY ("PatientId") REFERENCES "Patients" ("Id") ON DELETE CASCADE
+    );
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20230509230014_initial') THEN
+    CREATE INDEX "IX_Visits_Diagnosis" ON "Visits" ("Diagnosis");
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20230509230014_initial') THEN
+    CREATE INDEX "IX_Visits_PatientId" ON "Visits" ("PatientId");
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20230509230014_initial') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20230509230014_initial', '7.0.5');
+    END IF;
+END $EF$;
+COMMIT;
+
